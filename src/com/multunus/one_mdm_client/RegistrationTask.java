e35b0a4b57f9943c45d05a1b494c639906ff8055
Leena <leena.sn@multunus.com>
2014-10-23 12:42:28 +0530

    Move constants to a template file

--- src/com/multunus/one_mdm_client/RegistrationTask.java
+++ src/com/multunus/one_mdm_client/RegistrationTask.java
@@ -3,7 +3,6 @@ package com.multunus.one_mdm_client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 
 import org.apache.http.HttpResponse;
@@ -15,13 +14,11 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.content.Context;
 import android.os.AsyncTask;
-import android.util.Log;
 
 public class RegistrationTask extends AsyncTask<String, Void, String> {
 	
-	private static final String REGISTRATION_URL = "http://onemdm.herokuapp.com/device/create";
+	private static final String REGISTRATION_URL = Config.SERVER+"device/create";
 	private OneMDMService service;
 
 	public RegistrationTask(OneMDMService service) {
