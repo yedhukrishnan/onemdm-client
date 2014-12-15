e3a08fb804390177f5282f8898b5c78b67ab8a3f
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2014-12-15 12:23:42 +0530

    Bug Fix: Script polling stops when the server is not responding

--- src/main/java/com/multunus/one_mdm_client/ScriptListener.java
+++ src/main/java/com/multunus/one_mdm_client/ScriptListener.java
@@ -15,7 +15,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -86,33 +85,26 @@ public class ScriptListener {
         HttpResponse response = null;
         try {
             response = client.execute(request);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        response.getStatusLine().getStatusCode();
+
+        StringBuffer sb = new StringBuffer("");
 
         try {
             in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        StringBuffer sb = new StringBuffer("");
-        String l = "";
-        String nl = System.getProperty("line.separator");
-        try {
+
+            String l = "";
+            String nl = System.getProperty("line.separator");
             while ((l = in.readLine()) != null) {
                 sb.append(l + nl);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
             in.close();
-        } catch (IOException e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
         script = sb.toString();
-
         Log.d("one-mdm", script);
 
         return new JSONObject(script);
